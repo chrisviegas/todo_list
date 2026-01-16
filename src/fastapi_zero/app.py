29a3515448f6674e620453d0d167771e19e679e5
Christian Viegas <chris.gabriel346@gmail.com>
2026-01-15 22:52:36 -0300

    refactor: Updating the endpoints to use the real DB

--- src/fastapi_zero/app.py
+++ src/fastapi_zero/app.py
@@ -1,10 +1,14 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
 
+from src.fastapi_zero.database import get_session
+from src.fastapi_zero.models import User
 from src.fastapi_zero.schemas import (
     Message,
-    UserDB,
     UserList,
     UserPublic,
     UserSchema,
@@ -21,58 +25,88 @@ def read_root():
 
 
 @app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
 
-    database.append(user_with_id)
+    db_user = session.scalar(select(User).where(User.email == user.email))
 
-    return user_with_id
+    if db_user:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT,
+            detail="Already exists a user with this email.",
+        )
+    db_user = User(**user.model_dump())
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get("/users/", status_code=HTTPStatus.OK, response_model=UserList)
-def get_all_users():
-    return {"users": database}
+def get_all_users(
+    offset: int = 0, limit: int = 10, session: Session = Depends(get_session)
+):
+    users = session.scalars(select(User).limit(limit).offset(offset))
+    return {"users": users}
 
 
 @app.get(
     "/users/{user_id}", status_code=HTTPStatus.OK, response_model=UserPublic
 )
-def get_user(user_id: int):
+def get_user(user_id: int, session: Session = Depends(get_session)):
+    user_db = session.scalar(select(User).where(User.id == user_id))
 
-    if user_id < 1 or user_id > len(database):
+    if not user_db:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
-            detail=f"User with id {user_id} does not exist in the database.",
+            detail="User not found.",
         )
 
-    return database[user_id - 1]
+    return user_db
 
 
 @app.put(
     "/users/{user_id}", status_code=HTTPStatus.OK, response_model=UserPublic
 )
-def update_user(user_id: int, user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
+def update_user(
+    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+):
+    user_db = session.scalar(select(User).where(User.id == user_id))
 
-    if user_id < 1 or user_id > len(database):
+    if not user_db:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
-            detail=f"User with id {user_id} does not exist in the database.",
+            detail="User not found.",
         )
 
-    database[user_id - 1] = user_with_id
+    try:
+        user_db.email = user.email
+        user_db.name = user.name
+        user_db.password = user.password
 
-    return user_with_id
+        session.add(user_db)
+        session.commit()
+        session.refresh(user_db)
+
+        return user_db
+    except IntegrityError:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT,
+            detail="Already exists a user with this email.",
+        )
 
 
 @app.delete(
-    "/users/{user_id}", status_code=HTTPStatus.OK, response_model=UserPublic
+    "/users/{user_id}", status_code=HTTPStatus.OK, response_model=Message
 )
-def delete_user(user_id: int):
-    if user_id < 1 or user_id > len(database):
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    user_db = session.scalar(select(User).where(User.id == user_id))
+
+    if not user_db:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
-            detail=f"User with id {user_id} does not exist in the database.",
+            detail="User not found.",
         )
-
-    return database.pop(user_id - 1)
+    session.delete(user_db)
+    return {"message": "User deleted."}
