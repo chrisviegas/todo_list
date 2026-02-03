acf22369bb5b1af1bd95f1319e443b88bde4e7d9
Christian Viegas <chris.gabriel346@gmail.com>
2026-02-02 21:31:40 -0300

    Deploy on fly.io

--- src/todo_list/database.py
+++ src/todo_list/database.py
@@ -2,7 +2,12 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 from src.todo_list.settings import Settings
 
-engine = create_async_engine(Settings().DATABASE_URL)  # pyright: ignore[reportCallIssue]
+engine = create_async_engine(
+    Settings().DATABASE_URL,  # pyright: ignore[reportCallIssue]
+    max_overflow=10,
+    pool_size=5,
+    pool_recycle=200,
+)
 
 
 async def get_session():  # pragma: nocover
